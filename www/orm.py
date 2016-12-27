e2cb555caa124796a8c84fb2a016cf3018834ea7
windyear <1280027646@qq.com>
2016-12-27 21:05:53 +0800

    Just forgetting to add the orm code.

--- www/orm.py
+++ www/orm.py
@@ -16,7 +16,7 @@ async def create_pool(loop,**kw):
     __pool =await aiomysql.create_pool(host=kw.get('host','localhost'),
         port=kw.get('port',3306),user=kw['user'],password=kw['password'],
         db=kw['db'],charset=kw.get('charset','utf8'),
-        autosommit=kw.get('autocommit',True),maxsize=kw.get('maxsize',10),
+        autocommit=kw.get('autocommit',True),maxsize=kw.get('maxsize',10),
         minsize=kw.get('minsize',1),loop=loop)
 #查询语句的执行函数
 async def select(sql,args,size=None):
@@ -104,7 +104,7 @@ class ModelMetaclass(type):
             return type.__new__(cls,name,bases,attrs)
         tableName=attrs.get('__table__',None) or name
         logging.info('found model: %s (table: %s)' % (name,tableName))
-        mappingsdict()
+        mappings=dict()
         fields=[]
         primaryKey=None
         for k,v in attrs.items():
@@ -162,4 +162,72 @@ class Model(dict,metaclass=ModelMetaclass):
                 setattr(self,key,value)
         return value
         
+    @classmethod
+    async def findAll(cls,where=None,args=None,**kw):
+        ' find object by where clause.'
+        sql=[cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args=[]
+        orderBy=kw.get('orderBy',None)
+        if orderBy:
+            sql.append('orderBy')
+            sql.append(orderBy)
+        limit=kw.get('limit',None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit,int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit,tuple) and len(limit)==2:
+                sql.append('?,?')
+                args.append(limit)
+            else:
+                raise ValueError('Invalid limit value: %s'% str(limit))
+        rs=await select(''.join(sql),args)
+        return [cls(**r) for r in rs]
     
+    @classmethod
+    async def findNumber(cls,selectField,where=None,args=None):
+        ' find number by select and where.'
+        sql=['select %s _num_ from `%s`' % (selectField,cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs=await select(''.join(sql),args,1)
+        if len(rs)==0:
+            return None
+        return rs[0]['_num_']
+        
+    @classmethod
+    async def find(cls,pk):
+        'find object by primary key.'
+        rs=await select('%s where `%s'=?' % (cls.__select__,cls.__primary_key__),[pk],1)
+        if len(rs)==0:
+            return None
+        return cls(**rs[0])
+    @classmethod
+    async def save(self):
+        args=list(map(self.getValueOrDefault,self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows=await execute(self.__insert__,args)
+        if rows !=1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+    
+    @classmethod
+    async def update(self):
+        args=list(map(self.getValue,self.__fields__))
+        args.append(self,getValue(self.__primary_key__))
+        rows=await execute(self.__update__,args)
+        if rows!=1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+            
+    @classmethod
+    async def remove(self):
+        args=[self.getValue(self.__primary_key__)]
+        rows=await execute(self.__delete__,args)
+        if rows !=1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+        
